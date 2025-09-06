477682701ba8c0ecbcd6be12cce8d7ad134bb08e
RosaBen <benchabane.rosa@outlook.com>
2025-09-07 01:39:23 +0200

    added routes

---
+++ src/Router.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+
+export default function AppRouter(){
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/projects" element={<Projects/>} /> 
+      </Routes>
+    </Router>
+  );
+}
