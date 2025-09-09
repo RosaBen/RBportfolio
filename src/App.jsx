af64069b444535df9982cc344dbde9e7d8c16467
RosaBen <benchabane.rosa@outlook.com>
2025-09-09 13:03:08 +0200

    added html for form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routes/Router";
 import "./styles/navbar.css";
 import './styles/sampleProjects.css';
+import './styles/contact.css';
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,22 +3,30 @@ export default function Contact () {
   return (
     <>
       <section>
-        <p>Would you like to contact me to discuss about my projects? Please, click on "Leave a message" and fill out the form, I will contact you as soon as possible</p>
-        <button>Leave a Message</button>
-        <form action="">
-          <label htmlFor="fullname">Your name</label>
-          <input type="text" name="fullname" />
-          <label htmlFor="title">Object</label>
-          <input type="text" name="title" />
-          <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+        <div className="contact-container">
+          <div className="message-to-contact">
+            <p>Would you like to contact me to discuss about my projects? Please, click on "Leave a message" and fill out the form, I will contact you as soon as possible</p>
+            <button>Leave a Message</button>
+          </div>
           <fieldset>
-            <legend>
-              Your message
-            </legend>
-            <textarea name="content" id="content"></textarea>
+            <legend>Please Fill out form</legend>
+            <form action="">
+              <label htmlFor="fullname">Your name</label>
+              <input type="text" name="fullname" />
+              <label htmlFor="title">Object</label>
+              <input type="text" name="title" />
+              <label htmlFor="email">Email</label>
+              <input type="email" name="email" id="email" />
+              <fieldset>
+                <legend>
+                  Your message
+                </legend>
+                <textarea name="content" id="content"></textarea>
+              </fieldset>
+              <button type="submit">Send</button>
+            </form>
           </fieldset>
-        </form>
+        </div>
 
       </section>
     </>
